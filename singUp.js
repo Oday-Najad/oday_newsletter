5a629298ae027b6966942e2decb955d442cd4db2
Oday Najad <oday.najad@lau.edu>
2021-04-30 15:07:30 +0300

    way better

--- singUp.js
+++ singUp.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
-app.get("/index",(req,res)=>{
+app.get("/",(req,res)=>{
     
     res.sendFile(__dirname+"/index.html");
 
